646b09bfec3dd6eded5e495f53392b627991d90f
Luca Maciel <lucapmsantos50@gmail.com>
2026-04-13 17:37:08 -0300

    Use Random in Alteracao and add TestRandom
    Replace the custom LCG-based RNG in Alteracao.java with java.util.Random (seed 0) and simplify character substitution by using String.replace instead of a manual loop. Add TestRandom.java to show the deterministic random sequence, and include compiled .class files plus sample input/output files (pub.in, pub.out, mypub*.out) for testing.

--- tps/TP01/Alteracao.java
+++ tps/TP01/Alteracao.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Alteracao {
     public static boolean isFim(String s) {
@@ -7,26 +8,14 @@ public class Alteracao {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int seed = 4;
+        Random random = new Random(0);
 
         String s0 = sc.nextLine();
         while (!isFim(s0)) {
-            String resultado = "";
+            char letra1 = (char) ('a' + random.nextInt(26));
+            char letra2 = (char) ('a' + random.nextInt(26));
 
-            // Sorteio das letras sem usar Random
-            seed = (seed * 1103515245 + 12345) & 0x7fffffff;
-            char letra1 = (char) ('a' + (seed % 26));
-
-            seed = (seed * 1103515245 + 12345) & 0x7fffffff;
-            char letra2 = (char) ('a' + (seed % 26));
-
-            for (int i = 0; i < s0.length(); i++) {
-                if (s0.charAt(i) == letra1) {
-                    resultado = resultado + letra2;
-                } else {
-                    resultado = resultado + s0.charAt(i);
-                }
-            }
+            String resultado = s0.replace(letra1, letra2);
 
             System.out.println(resultado);
             s0 = sc.nextLine();
