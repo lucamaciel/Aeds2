ec108fd6aa79cf6c0177c47ee52d4cf84431b29c
Luca Maciel <lucapmsantos50@gmail.com>
2026-06-08 09:38:54 -0300

    Create Aula.java

---
+++ Anotações/Aula.java
@@ -0,0 +1,12 @@
+//implemente um metodo que retorne o maior elemento da ab
+
+
+/*nas totações duplas a segunda rotação eh a mais "relevante" comum. A 
+Seunda rotação acontece no avo. Faremos a rotação diresq diresq e a rotação esqdir, respectivamente
+nosn dois casos abaixo quando quando tivermos um "V" deitado.
+
+
+
+Pai e filho fiquem alinhados de forma certaf
+
+*/
